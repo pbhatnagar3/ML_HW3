5b6c5cd812edf5d90d5102bcb7ceca48877c3c13
Pujun Bhatnagar <pbhatnagar3@gatech.edu>
2015-04-05 18:35:22 -0400

    fixed SPECTRP.java

--- SPECTRP.java
+++ SPECTRP.java
@@ -19,28 +19,29 @@ public class SpectRP {
 
 	public static void main(String[] args){
 		int numInstances = 267;
-		int numAttributes = 23;
+		int numAttributes = 22;
 		String fileLocation = "src/Homework3/SPECT.txt";
     	Scanner scan = new Scanner(System.in);
     	System.out.println("Enter the number of reduced dimentions: ");
     	int numReducedDimentions = scan.nextInt();
+		// will create an array of all the instances
+		//since we will have to create a set from the instances
+		
 		DataSet dataSet = new DataSet((new CreateDataSet(numInstances, numAttributes, fileLocation).getIntanceArray()));
 		DataSet backupDataSet = new DataSet((new CreateDataSet(numInstances, numAttributes, fileLocation).getIntanceArray()));
-		System.out.println("Before Randomized projections");
-		System.out.println(dataSet);
+		System.out.println("Before Randomized projections\n");
+//		System.out.println(dataSet);
 		RandomizedProjectionFilter filter = new RandomizedProjectionFilter(numReducedDimentions, numAttributes);
 		System.out.println("Randomized vectors to which data will be projected");
 		System.out.println(filter.getProjection().transpose());
 		System.out.println("\n**********\n");
 		filter.filter(dataSet);
 		System.out.println("\n**********\n");
+		System.out.println("Projected Data");
 		System.out.println(dataSet);
 		System.out.println("\n**********\n");
-		filter.reverse(dataSet);
-		System.out.println("original values");
-		System.out.println(backupDataSet);
-		System.out.println("compare it to");
-		System.out.println("filtered and reverse filtered result");
-		System.out.println(dataSet);
+//		filter.reverse(dataSet);
+//		System.out.println(dataSet);
+		
 	}
 }
