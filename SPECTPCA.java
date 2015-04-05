e2556db550a0a44a28b7e884abe30eba4a843f36
Pujun Bhatnagar <pbhatnagar3@gatech.edu>
2015-04-05 02:44:47 -0400

    fixed SPECTPCA.java

--- SPECTPCA.java
+++ SPECTPCA.java
@@ -1,5 +1,6 @@
 package homework3;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import shared.DataSet;
@@ -14,36 +15,49 @@ import util.linalg.Matrix;
  * @version 1.0
  */
 public class SpectPCA {
-    
-    /**
-     * The test main
-     * @param args ignored
-     */
-    public static void main(String[] args) {
-    	int numInstances = 267;
-    	int numAttributes = 23;
-    	String fileLocation = "src/Homework3/SPECT.txt";
-    	Scanner scan = new Scanner(System.in);
-    	System.out.println("Enter the number of reduced dimentions: ");
-    	int numReducedDimentions = scan.nextInt();
-        DataSet set = new DataSet(new CreateDataSet(numInstances, numAttributes, fileLocation).getIntanceArray());
-        System.out.println("Before PCA");
-        System.out.println(set);
-        PrincipalComponentAnalysis filter = new PrincipalComponentAnalysis(set);
-        System.out.println(filter.getEigenValues());
-        System.out.println(filter.getProjection().transpose());
-        filter.filter(set);
-        System.out.println("After PCA");
-        System.out.println(set);
-        Matrix reverse = filter.getProjection().transpose();
-        for (int i = 0; i < set.size(); i++) {
-            Instance instance = set.get(i);
-            instance.setData(reverse.times(instance.getData()).plus(filter.getMean()));
-        }
-        System.out.println("After reconstructing");
-        System.out.println(set);
-        
-    }
+
+	/**
+	 * The test main
+	 * @param args ignored
+	 */
+	public static void main(String[] args) {
+		int numInstances = 267;
+		int numAttributes = 22;
+		String fileLocation = "src/Homework3/SPECT.txt";
+		DataSet set = new DataSet(new CreateDataSet(numInstances, numAttributes, fileLocation).getIntanceArray());
+		System.out.println("Before PCA");
+		//        System.out.println(set);
+		Scanner scan = new Scanner(System.in);
+		PrincipalComponentAnalysis filter = new PrincipalComponentAnalysis(set);
+		System.out.println("EIGENVALUE MATRIX: ");
+		System.out.println(filter.getEigenValues());
+		System.out.println("LIST OF EIGENVALUES: ");
+		ArrayList<Double> mainDiagonalValues = MainDiagonalValues.getMainDiagonal(filter.getEigenValues());
+		System.out.println(mainDiagonalValues);
+		System.out.println("EIGENVECTORS: ");
+		System.out.println(filter.getProjection().transpose());
+		System.out.println("projection matrix dimensions are " + filter.getProjection().m() + " and " + filter.getProjection().n());
+		System.out.println("enter the proposed number of eigenvectors to use: ");
+		filter.setReduceProjectionMatrixSize(scan.nextInt());
+		System.out.println(filter.getProjection().transpose());
+		System.out.println("projection matrix dimensions are " + filter.getProjection().m() + " and " + filter.getProjection().n());
+		System.out.println("does the updated eigenvector matrix look okay. Hit Y to continue: ");
+		scan.next();
+		System.out.println("TRANSFORMED DATA: ");
+		filter.filter(set);
+		System.out.println("After PCA");
+		System.out.println(set);
+		Matrix reverse = filter.getProjection().transpose();
+		for (int i = 0; i < set.size(); i++) {
+			Instance instance = set.get(i);
+			instance.setData(reverse.times(instance.getData()).plus(filter.getMean()));
+		}
+		//        System.out.println("After reconstructing");
+		//        System.out.println(set);
+
+	}
 
 }
+
+
 
